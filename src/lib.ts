72d223caea392159b798b1c2706c947a2498bf41
musienkoyuriy <musienkoyuriy@gmail.com>
2016-12-19 10:42:42 +0200

    add method for schema's creating. fixes

--- src/lib.ts
+++ src/lib.ts
@@ -1,45 +1,54 @@
 import { Injectable } from '@angular/core';
 import {
     Mapper,
+    Schema,
     Collection,
     LinkedCollection,
     Container,
     DataStore,
     Query
 } from 'js-data';
+
 import { HttpAdapter } from 'js-data-http';
+
+const DEFAULT_ADAPTER_OPTION: any = { 'default': true };
 
 @Injectable()
 export class Ng2JSData {
-  public createMapper(options: any, adapterOptions: any = { 'default': true }) {
-    let mapper = new Mapper(options);
+  public createMapper(options: any, adapterOptions: any = DEFAULT_ADAPTER_OPTION): any {
+    const mapper = new Mapper(options);
     const adapter = new HttpAdapter();
     mapper.registerAdapter('http', adapter, adapterOptions);
     return mapper;
   }
+
+  public createSchema(options: any, adapterOptions: any = DEFAULT_ADAPTER_OPTION): any {
+    const schema = new Schema(options);
+    return schema;
+  }
   
-  public createCollection(options: any, adapterOptions: any = { 'default': true }) {
-    let collection = new Collection(options);
+  public createCollection(options: any, adapterOptions: any = DEFAULT_ADAPTER_OPTION): any {
+    const collection = new Collection(options);
     return collection;
   }
   
-  public createLinkedCollection(options: any, adapterOptions: any = { 'default': true }) {
-    let linkedCollection = new LinkedCollection(options);
+  public createLinkedCollection(options: any, adapterOptions: any = DEFAULT_ADAPTER_OPTION): any {
+    const linkedCollection = new LinkedCollection(options);
     return linkedCollection;
   }
   
-  public createContainer(options: any, adapterOptions: any = { 'default': true }) {
-    let container = new Container(options);
+  public createContainer(options: any, adapterOptions: any = DEFAULT_ADAPTER_OPTION): any {
+    const container = new Container(options);
     return container;
   }
   
-  public createDataStore(options: any, adapterOptions: any = { 'default': true }) {
-    let ds = new DataStore(options);
+  public createDataStore(options: any, adapterOptions: any = DEFAULT_ADAPTER_OPTION): any {
+    const ds = new DataStore(options);
     return ds;
   }
   
-  public createQuery(options: any, adapterOptions: any = { 'default': true }) {
-    let query = new Query(options);
+  public createQuery(options: any, adapterOptions: any = DEFAULT_ADAPTER_OPTION): any {
+    const query = new Query(options);
     return query;
   }
 }
